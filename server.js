c6c08952e3ee7b8fd3b02e069b7c3df7917e0bf0
Lautaro Nievas <lautynievas09@gmail.com>
2022-10-25 21:04:43 -0300

    port change

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 require('dotenv').config();
 
+const port = process.env.PORT || 5000;
+
 // server used to send send emails
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen("https://floating-sea-59391.herokuapp.com", () => console.log("Server Running"));
+app.listen(port, () => console.log("Server Running"));
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   port: 587,
